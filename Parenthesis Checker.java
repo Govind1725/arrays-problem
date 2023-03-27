f0a98539ade228139417b81ea9f42fd7adcfcb10
Govind1725 <128210958+Govind1725@users.noreply.github.com>
2023-03-27 19:26:35 +0530

    Create Parenthesis Checker.java

---
+++ Parenthesis Checker.java
@@ -0,0 +1,35 @@
+{
+            Stack<Character> st = new Stack<>();
+
+            for(int i = 0;i < x.length();i++){
+                char ch = x.charAt(i);
+
+                if(ch == '(' || ch == '{' || ch == '['){
+                    st.push(ch);
+                }else if(ch == ')'){
+                    if(st.size() == 0 || st.peek() != '('){
+                        return false;
+                    }else{
+                        st.pop();
+                    }
+                }else if(ch == '}'){
+                    if(st.size() == 0 || st.peek() != '{'){
+                        return false;
+                    }else{
+                        st.pop();
+                    }
+                }else if(ch == ']'){
+                    if(st.size() == 0 || st.peek() != '['){
+                        return false;
+                    }else{
+                        st.pop();
+                    }
+                }
+            }
+
+            if(st.size() != 0){
+                return false;
+            }else{
+                return true;
+            }
+        }
