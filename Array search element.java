f13e70af5467d95bd1be066d4f6d43a831ad5e1b
Govind1725 <128210958+Govind1725@users.noreply.github.com>
2023-03-28 16:47:27 +0530

    Create Array search element.java

---
+++ Array search element.java
@@ -0,0 +1,11 @@
+ {
+        
+        // Your code here
+        int res=0;
+        for(int i=0; i<N; i++){
+            if(arr[i]==X){
+                return i ;
+            }
+        }
+        return -1;
+    }
