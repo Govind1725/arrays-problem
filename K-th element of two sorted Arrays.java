2628daf22e6fff5deaec4cc6e96b1754be7786aa
Govind1725 <128210958+Govind1725@users.noreply.github.com>
2023-03-28 12:58:29 +0530

    Create K-th element of two sorted Arrays.java

---
+++ K-th element of two sorted Arrays.java
@@ -0,0 +1,27 @@
+public class Govindh {
+    public static void main(String arg[]) {
+        int a[]= {1,4,6,8,9,};
+        int b[]= {2,3,5,7,10};
+        int s =5;
+        int n=a.length;
+        int m=b.length;
+        int c[]=new int[n+m];
+        int i=0,j=0,k=0;
+        while ( i < n && j< m){
+            if(a[i]< b[j]){
+                c[k++]= a[i++];
+            }
+            else {
+                c[k++] = b[j++];
+            }
+        }
+        while(i < n){
+            c[k++] = a[i++];
+        }
+        while(j < m){
+            c[k++] = b[j++];
+        }
+        System.out.println(c[s-1]);
+        }
+
+    }
